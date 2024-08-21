adf44861e041a54ce4427bbf423eaf9f5e509d9e
Joaquimtxt <jocaq124@gmail.com>
2024-08-21 14:56:11 -0300

    Fazendo o básico de uma lista

--- EstruturaDeDados1/Program.cs
+++ EstruturaDeDados1/Program.cs
@@ -2,8 +2,51 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Minha primeira branch!");
-        Console.WriteLine("Trabalhando com Git/Github");
-        Console.WriteLine("Mais uma tentativa de trabalhar com o Git");
+        //TRABALHANDO COM LISTA (LIST)
+        List<string> frutas = new List<string> { "Morango", "Manga", "Goiaba", "Uva" };
+
+
+        //Imprimir os itens na listas
+        Console.WriteLine("Minha Lista de Frutas:");
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine(" ");A
+
+        //Imprimir elemento na posição específica
+        Console.WriteLine("Fruta no índice 2:" + frutas[2]);
+
+        //Inserir um elemento na posição específica
+        frutas.Insert(1, "Maracujá");
+        Console.WriteLine(" ");
+        //Imprimir os itens na listas
+        Console.WriteLine("Minha Lista de Frutas:");
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine(" ");
+
+        //Alterar um elemento no índice específico
+        frutas[4] = "Pêra";
+        Console.WriteLine(" ");
+        //Imprimir os itens na listas
+        Console.WriteLine("Minha Lista de Frutas:");
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine(" ");
+
+        //Remover elementos da lista no índice específico
+        frutas.RemoveAt(3);
+        //Remover elementos pelo valor do conteúdo
+        frutas.Remove("Morango");
+
+        //Imprimir os itens na listas
+        Console.WriteLine("Minha Lista de Frutas:");
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine(" ");
+
+        //Apagar todos os elementos da lista
+        frutas.Clear();
+        //Imprimir os itens na listas
+        Console.WriteLine("Minha Lista de Frutas:");
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine(" ");
+
+
     }
 }
