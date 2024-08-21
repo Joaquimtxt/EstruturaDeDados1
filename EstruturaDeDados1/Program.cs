965e0eed514616a53b1ba4fb47a0720870621d56
Joaquimtxt <jocaq124@gmail.com>
2024-08-21 10:38:43 -0300

    Trabalhando com Branchs, teste 2

--- EstruturaDeDados1/Program.cs
+++ EstruturaDeDados1/Program.cs
@@ -2,6 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Minha primeira branch!");
+        Console.WriteLine("Trabalhando com Git/Github");
+        Console.WriteLine("Mais uma tentativa de trabalhar com o Git");
     }
 }
